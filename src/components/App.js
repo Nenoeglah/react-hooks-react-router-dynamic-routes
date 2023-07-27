fc9653479ea2794909eb6b1a8b4ca4cbe6eab8d0
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-07-27 20:30:55 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,46 @@
+// import React, { useState } from "react";
+// import { Route, Switch } from "react-router-dom";
+// import NavBar from "./NavBar";
+// import MoviesPage from "./MoviesPage";
+
+// function App() {
+//   const [movies, setMovies] = useState([
+//     { id: 1, title: "A River Runs Through It" },
+//     { id: 2, title: "Se7en" },
+//     { id: 3, title: "Inception" }
+//   ]);
+
+//   return (
+//     <div>
+//       <NavBar />
+//       <Switch>
+//         <Route path="/movies">
+//           <MoviesPage movies={movies} />
+//         </Route>
+//         <Route exact path="/">
+//           <div>Home</div>
+//         </Route>
+//       </Switch>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import MoviesPage from "./MoviesPage";
 
 function App() {
-  const [movies, setMovies] = useState([
+  const [movies] = useState([
     { id: 1, title: "A River Runs Through It" },
     { id: 2, title: "Se7en" },
     { id: 3, title: "Inception" }
   ]);
+
+  // Use setMovies to update the state of movies in the future
+  // setMovies([...movies, { id: 4, title: "New Movie" }]);
 
   return (
     <div>
